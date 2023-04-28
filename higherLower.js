7c91e97e287cc0eb0808eb033ca0fc1ca22d67c9
Steve M <stm0912@gmail.com>
2023-04-28 19:41:12 -0400

    Finished with higerlower enhancement

--- higherLower.js
+++ higherLower.js
@@ -17,9 +17,13 @@ while (valid_max != true) {
 
 let num = Math.floor(Math.random() * N) + 1;
 
+let guesses = []
+
 console.log(num)
 
 function the_guess() {
+
+    
 
     let guess = Number(document.getElementById("guess").value);
 
@@ -33,19 +37,32 @@ function the_guess() {
     else if (guess <= 0 || guess > N) {
 
         message.innerHTML = "That number is not in range, try again."
+        
+    }
+
+    else if (guesses.includes(guess)) {
+
+        message.innerHTML = `You already guessed ${guess}<br>Guess a different number.`
     }
 
     else if (guess == num) {
 
-        message.innerHTML = "You got it!"
+        guesses.push(guess)
+        numOfguesses = guesses.length
+        message.innerHTML = `You got it! It took you ${numOfguesses} tries and your guesses were ${guesses.join(", ")}`
+        
         }
     else if (guess < num) {
 
         message.innerHTML = "No, try a higher number."
+        guesses.push(guess)
+
         }
     else  if (guess > num) {
 
         message.innerHTML = "No, try a lower number."
+        guesses.push(guess)
+
 
         }
 }
